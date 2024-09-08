8e56206b466cddc6abccca80a869fc8ac459a430
Dhiraj <rd6999787@gmail.com>
2024-09-08 14:59:56 +0530

    cors origin changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS middleware for Express
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://codeshare-frontend-dun.vercel.app/",
   methods: ["GET", "POST"],
 }));
 
